78398b85db1a3eabf1874ab8588692862275474b
Alexander Minkin <weryskok@gmail.com>
2024-01-17 17:16:36 +0300

    fix(php): boost phpfpm

--- hosts/srv04/services/php.nix
+++ hosts/srv04/services/php.nix
@@ -9,10 +9,10 @@
     settings = {
       "pm" = "dynamic";
       "listen.owner" = config.services.nginx.user;
-      "pm.max_children" = 5;
+      "pm.max_children" = 32;
       "pm.start_servers" = 2;
-      "pm.min_spare_servers" = 1;
-      "pm.max_spare_servers" = 3;
+      "pm.min_spare_servers" = 2;
+      "pm.max_spare_servers" = 5;
       "pm.max_requests" = 500;
     };
   };
