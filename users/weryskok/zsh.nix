18ede776c3f7efd247df130afb008b435e2558f1
Alexander Minkin <weryskok@gmail.com>
2025-05-18 21:31:20 +0300

    feat: add mkhash fn for zsh, also add fzf

--- users/weryskok/zsh.nix
+++ users/weryskok/zsh.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   programs.zsh = {
     enable = true;
 
@@ -18,6 +23,16 @@
 
     initContent = ''
       source ~/.p10k.zsh
+
+      function mkhash() {
+        if [[ -z "$1" ]]; then
+          echo "Usage: mkhash <url>"
+          return 1
+        fi
+        local hash
+        hash=$(nix-prefetch-url "$1" --type sha256 --unpack)
+        nix hash to-sri sha256:"$hash"
+      }
     '';
 
     syntaxHighlighting.enable = true;
