21e186c1da57bcbb746cef31f84511894c212669
Alexander Minkin <weryskok@gmail.com>
2025-06-30 00:29:30 +0300

    feat: bring back backups

--- hosts/srv05/services/backups.nix
+++ hosts/srv05/services/backups.nix
@@ -16,33 +16,32 @@ in
     rclone
   ];
 
-  # systemd.services.backups = {
-  #   description = "Rustic backups";
-  #   after = [ "syslog.target" "network-online.target" "run-agenix.d.mount" ];
-  #   serviceConfig.EnvironmentFile = config.age.secrets.rustic-config.path;
-  #   serviceConfig.Type = "oneshot";
-  #   serviceConfig.User = "root";
-  #   path = [pkgs.rclone];
-  #   script = ''
-  #     set -eu
-  #     set -o pipefail
-  #     ${pkgs.rustic-rs}/bin/rustic backup /srv/pterodactyl --tag pterodactyl
-  #     ${pkgs.rustic-rs}/bin/rustic backup /var/lib/pterodactyl --tag wings
-  #     ${pkgs.rustic-rs}/bin/rustic backup /var/vmail --tag email
-  #     ${pkgs.rustic-rs}/bin/rustic backup /var/lib/mediawiki --tag mediawiki
-  #     ${config.services.mysql.package}/bin/mysqldump --user dumper --password=dump --databases murssite phpmyadmin mediawiki | ${pkgs.rustic-rs}/bin/rustic backup --stdin-filename database.sql - --tag database
-  #   '';
-  # };
+  systemd.services.backups = {
+    description = "Rustic backups";
+    after = [ "syslog.target" "network-online.target" "run-agenix.d.mount" ];
+    serviceConfig.EnvironmentFile = config.age.secrets.rustic-config.path;
+    serviceConfig.Type = "oneshot";
+    serviceConfig.User = "root";
+    path = [ pkgs.rclone ];
+    script = ''
+      set -eu
+      set -o pipefail
+      ${pkgs.rustic-rs}/bin/rustic backup /srv/pterodactyl --tag pterodactyl
+      ${pkgs.rustic-rs}/bin/rustic backup /var/lib/pterodactyl --tag wings
+      ${pkgs.rustic-rs}/bin/rustic backup /var/vmail --tag email
+      ${pkgs.rustic-rs}/bin/rustic backup /var/lib/mediawiki --tag mediawiki
+      ${config.services.mysql.package}/bin/mysqldump --user dumper --password=dump --databases murssite phpmyadmin mediawiki | ${pkgs.rustic-rs}/bin/rustic backup --stdin-filename database.sql - --tag database
+    '';
+  };
+  systemd.timers.backups = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 5:00:00 Europe/Moscow";
+      Persistent = true;
+      Unit = "backups.service";
+    };
 
-  # systemd.timers.backups = {
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     OnCalendar = "*-*-* 5:00:00 Europe/Moscow";
-  #     Persistent = true;
-  #     Unit = "backups.service";
-  #   };
-
-  # };
+  };
 
   # systemd.services.prunebackups = {
   #   description = "Prune Rustic backups";
