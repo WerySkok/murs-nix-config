838c798c23928415e2367747845daad84b928aa9
Alexander Minkin <weryskok@gmail.com>
2025-11-21 02:09:27 +0300

    feat: add nginx and minecraft exporters for prometheus

--- hosts/srv05/services/prometheus.nix
+++ hosts/srv05/services/prometheus.nix
@@ -9,6 +9,10 @@
         enabledCollectors = [ "systemd" ];
         port = 9002;
       };
+      nginx = {
+        enable = true;
+        port = 9113;
+      };
     };
 
     scrapeConfigs = [
@@ -18,7 +22,19 @@
           targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
         }];
       }
+      {
+        job_name = "srv05_nginx";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.nginx.port}" ];
+        }];
+      }
+      {
+        job_name = "murs_minecraft";
+        static_configs = [{
+          targets = [ "172.18.0.1:25585" ];
+        }];
+      }
     ];
-
   };
+  services.nginx.statusPage = true;
 }
